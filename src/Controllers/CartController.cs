188a4ea48a33ed6357597a922a88ecfe71c878c8
Juan Osorio <jagojar@hotmail.com>
2026-01-14 13:52:29 -0500

    adding secrets

--- src/Controllers/CartController.cs
+++ src/Controllers/CartController.cs
@@ -5,6 +5,7 @@ namespace ZavaStorefront.Controllers
 {
     public class CartController : Controller
     {
+        string admin_password = "XXX";
         private readonly ILogger<CartController> _logger;
         private readonly CartService _cartService;
 
